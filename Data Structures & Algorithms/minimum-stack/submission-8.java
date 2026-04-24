5e4ac6ff075a5c0b0af31c9bfe8a48f6293db163
Sai Deepthi <132867368+SAIDEEPTHIT@users.noreply.github.com>
2026-04-24 17:24:23 +0530

    Add: minimum-stack - submission-8

---
+++ Data Structures & Algorithms/minimum-stack/submission-8.java
@@ -0,0 +1,26 @@
+class MinStack {
+    Stack <Integer> st=new Stack<>();
+    Stack <Integer> stMin=new Stack<>();
+    public MinStack() {
+        
+    }
+    
+    public void push(int val) {
+        st.push(val);
+        if (stMin.isEmpty() || !stMin.isEmpty() && val <= stMin.peek()) 
+            stMin.push(val);
+    }
+    
+    public void pop() {
+        if (st.peek().equals(stMin.peek())) stMin.pop();
+        st.pop();
+    }
+    
+    public int top() {
+        return st.peek();
+    }
+    
+    public int getMin() {
+        return stMin.peek();
+    }
+}
